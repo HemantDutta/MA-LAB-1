fd319f2b8889faaafb8a2d88820e459b650aaf7f
HemantDutta <hemantnorddutta2@gmail.com>
2023-07-07 00:58:06 +0530

    Finish Project

--- app/src/main/java/com/example/ma_lab_1/MainActivity.java
+++ app/src/main/java/com/example/ma_lab_1/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.ma_lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void greeting(View view){
+        EditText mEdit = findViewById(R.id.editTextText);
+        String data = mEdit.getText().toString();
+
+        Toast.makeText(this, "Hello "+data, Toast.LENGTH_SHORT).show();
     }
 }
